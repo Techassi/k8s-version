9a7372e5fa2ae2d6339e70e33a4d7aecdd5345df
Techassi <git@techassi.dev>
2024-01-30 00:03:27 +0100

    Add Display impl, remove Snafu dep, add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,12 @@
 use std::{
+    fmt::Display,
     iter::Peekable,
     str::{Bytes, FromStr},
 };
 
-use snafu::{ensure, Snafu};
+mod error;
 
-#[derive(Debug, Snafu)]
-pub enum Error {
-    #[snafu(display("unexpected char {character:?} at index {}"))]
-    IllegalChar { character: char, index: usize },
-
-    #[snafu(display("expected a string with 63 or less characters, got {length:?}"))]
-    IllegalLength { length: usize },
-
-    #[snafu(display("invalid version with non-ASCII characters"))]
-    NonAscii,
-
-    #[snafu(display("empty string, expected a Kubernetes resource version"))]
-    Empty,
-
-    #[snafu(display("integer overflow"))]
-    IntegerOverflow,
-
-    #[snafu(display("leading zero"))]
-    LeadingZero,
-
-    #[snafu(display("expected \"beta\" or \"alpha\" identifier, got {identifier:?}"))]
-    IllegalIdentifier { identifier: String },
-}
+use error::Error;
 
 /// A Kubernetes resource version with the `v<MAJOR>(beta/alpha<VERSION>)` format.
 #[derive(Debug)]
@@ -36,34 +15,33 @@ pub struct Version {
     pub minor: Option<Minor>,
 }
 
-#[derive(Debug)]
-pub enum Minor {
-    Beta(u64),
-    Alpha(u64),
-}
-
 impl FromStr for Version {
     type Err = Error;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        ensure!(!input.is_empty(), EmptySnafu);
+        if input.is_empty() {
+            return Err(Error::Empty);
+        }
 
         let length = input.len();
-        ensure!(length <= 63, IllegalLengthSnafu { length });
+        if length > 63 {
+            return Err(Error::IllegalLength { length });
+        }
 
-        ensure!(input.is_ascii(), NonAsciiSnafu);
+        if !input.is_ascii() {
+            return Err(Error::NonAscii);
+        }
 
         // TODO (Techassi): Handle this error
         let mut bytes = input.bytes().peekable();
         let first = bytes.next().unwrap();
 
-        ensure!(
-            first == b'v',
-            IllegalCharSnafu {
-                character: first,
-                index: 0usize
-            }
-        );
+        if first != b'v' {
+            return Err(Error::IllegalChar {
+                character: first as char,
+                index: 0,
+            });
+        }
 
         let major = extract_digits(&mut bytes)?;
 
@@ -74,7 +52,11 @@ impl FromStr for Version {
         let minor = match extract_chars(&mut bytes).as_str() {
             "beta" => Minor::Beta(extract_digits(&mut bytes)?),
             "alpha" => Minor::Alpha(extract_digits(&mut bytes)?),
-            identifier => return IllegalIdentifierSnafu { identifier }.fail(),
+            identifier => {
+                return Err(Error::IllegalIdentifier {
+                    identifier: identifier.into(),
+                })
+            }
         };
 
         Ok(Self {
@@ -84,13 +66,44 @@ impl FromStr for Version {
     }
 }
 
+impl Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.minor {
+            Some(minor) => write!(f, "v{}{}", self.major, minor),
+            None => write!(f, "v{}", self.major),
+        }
+    }
+}
+
+impl Version {
+    pub fn new(major: u64, minor: Option<Minor>) -> Self {
+        Self { major, minor }
+    }
+}
+
+/// A minor Kubernetes resource version with the `beta/alpha<VERSION>` format.
+#[derive(Debug)]
+pub enum Minor {
+    Beta(u64),
+    Alpha(u64),
+}
+
+impl Display for Minor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Minor::Beta(beta) => write!(f, "beta{}", beta),
+            Minor::Alpha(alpha) => write!(f, "alpha{}", alpha),
+        }
+    }
+}
+
 fn extract_digits(bytes: &mut Peekable<Bytes<'_>>) -> Result<u64, Error> {
-    let mut at_start = false;
+    let mut at_start = true;
     let mut number = 0u64;
 
     while let Some(digit) = bytes.next_if(|b| (*b >= b'0' && *b <= b'9')) {
         if at_start && digit == b'0' {
-            return LeadingZeroSnafu.fail();
+            return Err(Error::LeadingZero);
         }
         at_start = false;
 
@@ -99,7 +112,7 @@ fn extract_digits(bytes: &mut Peekable<Bytes<'_>>) -> Result<u64, Error> {
             .and_then(|v| v.checked_add((digit - b'0') as u64))
         {
             Some(sum) => number = sum,
-            None => return IntegerOverflowSnafu.fail(),
+            None => return Err(Error::IntegerOverflow),
         }
     }
 
@@ -122,11 +135,23 @@ mod test {
     use rstest::rstest;
 
     #[rstest]
+    #[case("v1alpha12")]
     #[case("v1alpha1")]
     #[case("v1beta1")]
     #[case("v1")]
-    fn test_from_str(#[case] input: &str) {
+    fn valid_from_str(#[case] input: &str) {
         let version = Version::from_str(input).unwrap();
-        println!("{version:?}")
+        assert_eq!(version.to_string(), input);
+    }
+
+    #[rstest]
+    #[case("v1gamma12", Error::IllegalIdentifier { identifier: "gamma".into() })]
+    #[case("1beta1", Error::IllegalChar { character: '1', index: 0 })]
+    #[case("v1beta0", Error::LeadingZero)]
+    #[case("v1betä1", Error::NonAscii)]
+    #[case("", Error::Empty)]
+    fn invalid_from_str(#[case] input: &str, #[case] error: Error) {
+        let err = Version::from_str(input).unwrap_err();
+        assert_eq!(err, error)
     }
 }
